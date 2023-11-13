74fc44c066c197e9efdc28629e99c21c439c0534
filecc <fil.gia@live.it>
2023-11-13 17:05:31 +0100

    Refactor API controller code

--- controllers/api.js
+++ controllers/api.js
@@ -3,42 +3,48 @@ const fs = require("fs");
 const path = require("path");
 const env = require("dotenv").config();
 
-const port = process.env.PORT ?? ''
-const host = process.env.HOST.includes('localhost') ? 'localhost' : ('https://' + process.env.HOST + '/')
+const port = process.env.PORT ?? "";
+const host = process.env.HOST.includes("localhost")
+  ? "localhost"
+  : "https://" + process.env.HOST + "/";
 
-const posts = JSON.parse(fs.readFileSync(path.resolve("./db/posts.json"), "utf8"))
+const posts = JSON.parse(
+  fs.readFileSync(path.resolve("./db/posts.json"), "utf8")
+);
 
 /**
- * @param {express.Request} req 
- * @param {express.Response} res 
+ * @param {express.Request} req
+ * @param {express.Response} res
  */
 
-function index (req, res) {
-    if(!posts){
-        res.status(404).json({error: 404, message: "Posts not found"})
-        return
-    }
-   
-    res.json(posts)
+function index(req, res) {
+  if (!posts) {
+    res.status(404).json({ error: 404, message: "Posts not found" });
+    return;
+  }
+
+  res.json(posts);
 }
 
 function show(req, res) {
-    const post = posts.find(post => post.id === req.params.id)
+  const post = posts.find((post) => post.id === req.params.id);
 
-   
-
-if(post){
-    const imgPath = (`http://${host}:${port}/images${post.image}`)
-    const downloadLink= (`http://localhost:3000/posts/${post.slug}/download`)
-    res.json({...post, image_url: `${imgPath}`, download_link: `${downloadLink}`})
+  if (!post) {
+    res
+      .status(404)
+      .send({ error: 404, message: `Post with id ${req.params.id} not found` });
+    return;
+  }
+  const imgPath = `http://${host}:${port}/images${post.image}`;
+  const downloadLink = `http://localhost:3000/posts/${post.slug}/download`;
+  res.json({
+    ...post,
+    image_url: `${imgPath}`,
+    download_link: `${downloadLink}`,
+  });
 }
-    res.status(404).send({error: 404, message: `Post with id ${req.params.id} not found`})
-    return
-}
-
-
 
 module.exports = {
-    index,
-    show
-  }
+  index,
+  show,
+};
